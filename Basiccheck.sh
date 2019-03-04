6e5197afb8266713dd5ac704c4891931caba9b7a
Timor Sharabi <xposionn@gmail.com>
2019-03-04 16:56:00 +0200

    first commit

---
+++ Basiccheck.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+dirname=$1
+exename=$2
+shift 2
+rest=$@
+cdir=$(pwd)
+errid=0;
+cd
+cd $dirname
+make
+lans=$?
+if [ $lans -eq 0 ]; then
+		valgrind --leak-check=full --error-exitcode=1 ./$exename $rest
+		valerr=$?
+		valgrind --tool=helgrind --error-exitcode=1 ./$exename $rest
+		helerr=$?
+else
+		lans=1
+		valerr=1
+		helerr=1
+fi
+
+
+
+echo "Results: Compilation: " $lans "Memory Leak: " $valerr " Thread Race: " $helerr 
+
+lans=$((lans*4))
+valerr=$((valerr*2))
+
+errorID=$((lans+valerr+helerr))
+echo $errorID
+cd 
+cd $cdir
